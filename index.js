9f504b03d04b8cdca5f09b960ba179d14375c3c3
Makk2002 <43918343+Makk2002@users.noreply.github.com>
2019-03-04 18:19:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("<3", {type: "PLAYING"});
+  bot.user.setActivity(">pomoc", {type: "PLAYING"});
 
 });
 
@@ -42,30 +42,6 @@ bot.on("message", async message => {
     };
   }
 
-  if(!coins[message.author.id]){
-    coins[message.author.id] = {
-      coins: 0
-    };
-  }
-
-  let coinAmt = Math.floor(Math.random() * 15) + 1;
-  let baseAmt = Math.floor(Math.random() * 15) + 1;
-  console.log(`${coinAmt} ; ${baseAmt}`);
-
-  if(coinAmt === baseAmt){
-    coins[message.author.id] = {
-      coins: coins[message.author.id].coins + coinAmt
-    };
-  fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
-    if (err) console.log(err)
-  });
-  let coinEmbed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor("#0000FF")
-  .addField("**Galaretki**", `${coinAmt} Galaretki dodane!`);
-
-  message.channel.send(coinEmbed).then(msg => {msg.delete(2500)});
-  }
 
   let xpAdd = Math.floor(Math.random() * 8) + 15;
 
@@ -88,7 +64,7 @@ bot.on("message", async message => {
     .setColor("#cc0000")
     .addField("Nowy Poziom", curlvl + 1);
 
-    message.channel.send(lvlup).then(msg => {msg.delete(2500)});
+    //message.channel.send(lvlup).then(msg => {msg.delete(2500)});
   }
   fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
     if(err) console.log(err)
@@ -104,4 +80,4 @@ bot.on("message", async message => {
 
 });
 
-bot.login(botconfig.token);
+bot.login(botconfig.BOT_TOKEN);
