d63a6f34d03b90cf2c71972ece38bcb5d6f05eef
Makk2002 <43918343+Makk2002@users.noreply.github.com>
2019-03-04 18:16:54 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-const Komada = require("komada");
 const fs = require("fs");
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
+let coins = require("./coins.json");
 let xp = require("./xp.json");
 
 fs.readdir("./commands/", (err, files) => {
@@ -25,20 +25,48 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity(">pomoc", {type: "WATCHING"});
+  bot.user.setActivity("<3", {type: "PLAYING"});
 
 });
+
 
 bot.on("message", async message => {
 
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+      prefixes: botconfig.prefix
+    };
+  }
+
+  if(!coins[message.author.id]){
+    coins[message.author.id] = {
+      coins: 0
+    };
+  }
+
+  let coinAmt = Math.floor(Math.random() * 15) + 1;
+  let baseAmt = Math.floor(Math.random() * 15) + 1;
+  console.log(`${coinAmt} ; ${baseAmt}`);
+
+  if(coinAmt === baseAmt){
+    coins[message.author.id] = {
+      coins: coins[message.author.id].coins + coinAmt
+    };
+  fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+    if (err) console.log(err)
+  });
+  let coinEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#0000FF")
+  .addField("**Galaretki**", `${coinAmt} Galaretki dodane!`);
+
+  message.channel.send(coinEmbed).then(msg => {msg.delete(2500)});
+  }
+
   let xpAdd = Math.floor(Math.random() * 8) + 15;
 
   if(!xp[message.author.id]){
@@ -60,12 +88,20 @@ bot.on("message", async message => {
     .setColor("#cc0000")
     .addField("Nowy Poziom", curlvl + 1);
 
-    //message.channel.send(lvlup).then(msg => {msg.delete(100)});
+    message.channel.send(lvlup).then(msg => {msg.delete(2500)});
   }
   fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
     if(err) console.log(err)
   });
-  
+
+  let prefix = prefixes[message.guild.id].prefixes;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+
 });
 
-bot.login(process.env.BOT_TOKEN);
+bot.login(botconfig.token);
